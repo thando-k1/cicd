b9ea6069f2898965745531af711e2917aa9064e2
thando-k1 <Thandomk27@gmail.com>
2025-04-17 12:04:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('clickMeButton').addEventListener('click', function () {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = "Button Clicked! CI/CD is awesome!";
+});
